3b42f96aa3830a7ceb2922c83713a823e9a8cfa2
Elka Software <48525469+elkasoftwaree@users.noreply.github.com>
2023-10-09 17:21:26 +0300

    Initial Commit

---
+++ FirstSwiftUIApp/FirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIAppApp.swift
+//  FirstSwiftUIApp
+//
+//  Created by Kaan Ezerrtaş on 9.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
